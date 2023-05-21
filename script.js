2bb5a90eaf09d822fe410dfdc93166f5c71ed6b8
Romel Quintero <92559113+romelquintero@users.noreply.github.com>
2023-05-21 07:41:19 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const btn = document.querySelector(".share");
-const media = document.querySelector(".media");
-
-btn.addEventListener("click", function () {
-  media.classList.toggle("hidden");
-});
